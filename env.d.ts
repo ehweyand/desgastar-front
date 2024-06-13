f4672e61def274a05f2e4f3132bbd0275638e4a6
ehweyand <ehweyand@univates.br>
2024-06-13 16:03:36 -0300

    type fix for routing

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
+
+// types.d.ts
+declare module 'virtual:generated-layouts' {
+    import { RouteRecordRaw } from 'vue-router'
+    export function setupLayouts(routes: RouteRecordRaw[]): RouteRecordRaw[]
+}
